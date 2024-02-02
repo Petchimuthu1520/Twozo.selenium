616c0b8c6339f4c9d3939742a6709e6b9c0c9c0b
Petchimuthu1520 <petchi.muthu@thelaunchclub.in>
2024-02-02 16:29:52 +0530

    Update: Components base

---
+++ src/main/java/org/twozo/components/driver/BrowserType.java
@@ -0,0 +1,32 @@
+package org.twozo.components.driver;
+
+/**
+ * Enum representing different types of web browsers.
+ */
+enum BrowserType {
+
+    /**
+     * Google Chrome browser.
+     */
+    CHROME,
+
+    /**
+     * Mozilla Firefox browser.
+     */
+    FIREFOX,
+
+    /**
+     * Apple Safari browser.
+     */
+    SAFARI,
+
+    /**
+     * Internet Explorer browser.
+     */
+    INTERNET_EXPLORER,
+
+    /**
+     * Microsoft Edge browser.
+     */
+    EDGE;
+}
